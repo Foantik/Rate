60dc6dbf0dbfb7f3fd94c54274ba3a40de9426b2
Foantik <nasta3187@gmail.com>
2021-11-08 16:26:11 +0300

    clean

--- funcs.h
+++ funcs.h
@@ -6,19 +6,5 @@ void deystvie(double a,double b) {
     char dey;
     cin >> dey;
     double otvet;
-    switch (dey) {
-    case '+':
-        otvet = a + b;
-        cout << "Ответ: " << otvet << endl;
-        break;
-    case '-':
-        otvet = a - b;
-        cout << "Ответ: " << otvet << endl;
-        break;
-    default:
-        cout << "Неверный ввод";
-        otvet = 10;
-        break;
-    }
- 
+   
 }
