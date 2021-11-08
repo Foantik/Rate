63b5135ceef180d47d69a54c9dda40c6c2a2848d
Foantik <nasta3187@gmail.com>
2021-11-08 16:23:02 +0300

    Clone

--- funcs.h
+++ funcs.h
@@ -1,3 +1,24 @@
 #include <iostream>
 #include "windows.h"
 #include <cmath>
+using namespace std;
+void deystvie(double a,double b) {
+    char dey;
+    cin >> dey;
+    double otvet;
+    switch (dey) {
+    case '+':
+        otvet = a + b;
+        cout << "Ответ: " << otvet << endl;
+        break;
+    case '-':
+        otvet = a - b;
+        cout << "Ответ: " << otvet << endl;
+        break;
+    default:
+        cout << "Неверный ввод";
+        otvet = 10;
+        break;
+    }
+ 
+}
